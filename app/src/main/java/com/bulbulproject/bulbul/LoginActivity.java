9798eb5457fcf3c4376bb55007f98447b66b7f8b
Enes Akdogan <aeakdogan@gmail.com>
2017-02-12 12:04:30 +0300

    Register Activity Completed

--- app/src/main/java/com/bulbulproject/bulbul/LoginActivity.java
+++ app/src/main/java/com/bulbulproject/bulbul/LoginActivity.java
@@ -199,7 +199,7 @@ public class LoginActivity extends AppCompatActivity {
 
             try {
                 // Simulate network access.
-                Thread.sleep(2000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 return 0;
             }
