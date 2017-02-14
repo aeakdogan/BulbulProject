f12d9037cc3a6608c5e3fd19c56e7d1da14493de
Enes Akdogan <aeakdogan@gmail.com>
2017-02-14 08:43:29 +0300

    Followers and Following pages created

--- app/src/main/java/com/bulbulproject/bulbul/MyProfile.java
+++ app/src/main/java/com/bulbulproject/bulbul/MyProfile.java
@@ -1,13 +1,18 @@
 package com.bulbulproject.bulbul;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class MyProfile extends AppCompatActivity {
+
+    TextView followingsText;
+    TextView followersText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,24 @@ public class MyProfile extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        followersText = (TextView) findViewById(R.id.follower_count);
+        followersText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Followers.class);
+                startActivity(intent);
+            }
+        });
+
+        followingsText = (TextView) findViewById(R.id.following_count);
+        followingsText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Followings.class);
+                startActivity(intent);
             }
         });
     }
