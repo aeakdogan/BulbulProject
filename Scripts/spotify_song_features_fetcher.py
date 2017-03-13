668aef6bc0abbb57825c6a2a100015e513e0f9d8
Fatih Taş <fatihtas26@gmail.com>
2017-03-14 02:45:26 +0300

    Implemented Slack notifier Fixed authentication flow Added Too Many Requests response handler

--- Scripts/spotify_song_features_fetcher.py
+++ Scripts/spotify_song_features_fetcher.py
@@ -5,6 +5,9 @@
 ###################
 
 import requests
+import json
+import base64
+import atexit
 from time import sleep
 
 
@@ -37,18 +40,17 @@ def get_spotify_tracks(filename):
 # Downloads track audio analysis and features from spotify
 def get_audio_analysis_and_features(filename):
     data = get_spotify_uris(filename)
-    auth_token = 'BQAq8MiRRjac2k-ZspoBCZ1e0lpmE0WbMgfVnnSZZ0tlcCTgJBFSIvr8i3XSbKofP2EWwASBO7hi-vmumqIDEgUN6EbNtSKvvdPxL1wLmDo5Z3MjFgMdfxytv6sw9fMrl_Myz8stKPB1BjU9'
     counter = 0
+
+    notify_slack('Started running script')
 
     for id in data:
         analysis_url = 'https://api.spotify.com/v1/audio-analysis/{}'.format(id)
         features_url = 'https://api.spotify.com/v1/audio-features/{}'.format(id)
 
-        analysis_resp = requests.get(analysis_url, headers={'Authorization': 'Bearer {}'.format(auth_token),
-                                                            'Accept': 'application/json'})
+        analysis_resp = get_authenticated(analysis_url)
         sleep(0.2)
-        features_resp = requests.get(features_url, headers={'Authorization': 'Bearer {}'.format(auth_token),
-                                                            'Accept': 'application/json'})
+        features_resp = get_authenticated(features_url)
 
         with open('track_audio_analysis/{}_audio_analysis'.format(id), 'w') as f:
             f.write(analysis_resp.text)
@@ -59,7 +61,71 @@ def get_audio_analysis_and_features(filename):
         counter += 1
         print(counter)
 
+    notify_slack('Finished script')
 
-filename = 'matched_lastfm_spotify_ids.txt'
-# get_audio_analysis_and_features(filename)
+
+def refresh_auth_token():
+    global token
+    url = 'https://accounts.spotify.com/api/token'
+
+    payload = {
+        'grant_type': 'client_credentials',
+    }
+
+    resp = requests.post(url, data=payload, headers={'Authorization': 'Basic {}'.format(credentials)})
+
+    if resp.status_code == 200:
+        print(resp.json())
+        json_response = resp.json()
+        token = json_response['access_token']
+    else:
+        print(resp.json())
+        report_to_slack(resp)
+
+
+def get_authenticated(url):
+    response = requests.get(url, headers={'Authorization': 'Bearer {}'.format(token),
+                                          'Accept': 'application/json'})
+    if response.status_code == 429:
+        print('Sleeping for {} seconds'.format(response.headers['Retry-After']))
+        sleep(response.headers['Retry-After'])
+        return get_authenticated(url)
+
+    elif response.status_code == 401:
+        refresh_auth_token()
+        print(response.json())
+        return get_authenticated(url)
+
+    elif 200 <= response.status_code < 300:
+        return response
+
+    else:
+        report_to_slack(response)
+
+
+def notify_slack(message):
+    payload = {
+        'payload': json.dumps({'text': message})
+    }
+    print(message)
+    resp = requests.post(slack_webhook_url, data=payload)
+
+
+def report_to_slack(response):
+    notify_slack('Script stopped \n{} : \n{}'.format(response.json(), response.url))
+    exit(0)
+
+
+client_id = '4e262c64ec3a472e935cb6506dd661bf'
+client_secret = '88aaa1ebc11d4cffa99d4bb4bc37e0ac'
+filename = '../Data/matched_lastfm_spotify_ids.txt'
+slack_webhook_url = 'https://hooks.slack.com/services/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'
+credentials = base64.b64encode(bytes('{}:{}'.format(client_id, client_secret), 'utf-8')).decode('utf-8')
+atexit.register(notify_slack, message='Exited script')
+
+token = ''
+refresh_auth_token()
+get_audio_analysis_and_features(filename)
+
 # get_spotify_tracks(filename)
+# scope=user-read-private&scope=user-read-birthdate&scope=user-read-email&scope=playlist-read-private&scope=playlist-read-collaborative&scope=playlist-modify-public&scope=playlist-modify-private&scope=user-library-read&scope=user-library-modify&scope=user-follow-read&scope=user-follow-modify&scope=user-top-read
