6db294d6b50fd6700bd2710838c9cf20ace4fad4
omer58 <omerakgul58@gmail.com>
2017-04-19 02:36:29 +0300

    Update importbulbul.sh

--- csv-files/importbulbul.sh
+++ csv-files/importbulbul.sh
@@ -1,12 +1,21 @@
+export JAVA_OPTS="-Xmx4G"
+
 ./neo4j-import --into bulbul.db \
                 --id-type string \
                --multiline-fields=true \
+                --bad-tolerance=50000 \
                 --skip-duplicate-nodes=true \
-         --nodes:Album /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/albums.csv \
-         --nodes:Artist /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/artists.csv \
-         --nodes:Tag /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/tags.csv \
-         --nodes:Track /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/tracks.csv \
-         --relationships:BY /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/album-artist.csv \
-         --relationships:BY /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/track-artist.csv \
-         --relationships:HAS /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/album-track.csv \
-         --relationships:TAGGED /home/burak/Documents/SeniorProject/BulbulProject/BulbulProject/Data/artist-tags.csv
+                 --skip-bad-relationships=true \
+                --ignore-empty-strings =true \
+         --nodes:Album ~/Bulbul/Data/Data/albums.csv \
+         --nodes:Artist ~/Bulbul/Data/Data/artists.csv \
+         --nodes:Tag ~/Bulbul/Data/Data/tags.csv \
+         --nodes:Track ~/Bulbul/Data/Data/tracks.csv \
+         --nodes:BulbulUser ~/Bulbul/Data/Data/user-info-withpw.csv \
+         --relationships:BY ~/Bulbul/Data/Data/album-artist.csv \
+         --relationships:BY ~/Bulbul/Data/Data/track-artist.csv \
+         --relationships:HAS ~/Bulbul/Data/Data/album-track.csv \
+         --relationships:FOLLOWS ~/Bulbul/Data/Data/user-to-album.csv \
+         --relationships:FOLLOWS ~/Bulbul/Data/Data/user-to-artist.csv \
+         --relationships:FOLLOWS ~/Bulbul/Data/Data/user-to-track.csv \
+         --relationships:FOLLOWS ~/Bulbul/Data/Data/user-to-user.csv
