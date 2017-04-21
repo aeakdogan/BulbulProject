05d31742d5568af6d361db10ed7ba871f4358b98
Fatih Taş <fatihtas26@gmail.com>
2017-04-22 02:15:11 +0300

    Integrated MyAlbumsFragment with API

--- app/src/main/java/com/bulbulproject/bulbul/fragment/MyAlbumsFragment.java
+++ app/src/main/java/com/bulbulproject/bulbul/fragment/MyAlbumsFragment.java
@@ -1,5 +1,6 @@
 package com.bulbulproject.bulbul.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,13 +8,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.apollographql.android.ApolloCall;
+import com.apollographql.android.api.graphql.Response;
+import com.bulbulproject.UserAlbumsQuery;
+import com.bulbulproject.bulbul.App;
 import com.bulbulproject.bulbul.R;
+import com.bulbulproject.bulbul.activity.AlbumActivity;
 import com.bulbulproject.bulbul.adapter.AlbumsRVAdapter;
 import com.bulbulproject.bulbul.model.Album;
+import com.bulbulproject.bulbul.model.Artist;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.Nonnull;
 
 /**
  * Created by burak on 13.02.2017.
@@ -31,8 +41,7 @@ public class MyAlbumsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        initializeData();
+        albums = new ArrayList<Album>();
     }
 
     @Override
@@ -47,22 +56,41 @@ public class MyAlbumsFragment extends Fragment {
 
         rvAdapter = new AlbumsRVAdapter(albums, getContext());
         mRecyclerView.setAdapter(rvAdapter);
+        String token = getActivity().getApplication().getSharedPreferences(getString(R.string.shared_preferences), Context.MODE_PRIVATE).getString("AUTH_TOKEN", "");
+        ((App)getActivity().getApplication()).apolloClient().newCall(UserAlbumsQuery.builder().token(token).build()).enqueue(new ApolloCall.Callback<UserAlbumsQuery.Data>() {
+            @Override
+            public void onResponse(@Nonnull Response<UserAlbumsQuery.Data> response) {
+                if(response.isSuccessful()){
+                    UserAlbumsQuery.Data.User user = response.data().users().get(0);
+                    if(user.followedAlbums() != null){
+                        for(UserAlbumsQuery.Data.User.FollowedAlbum album: user.followedAlbums()){
+                            Album newAlbum = new Album(album.name(),0,album.image());
+                            newAlbum.setId(album.id());
+                            newAlbum.setSongsCount(album.tracksCount());
+                            if(album.artists() != null){
+                                for(UserAlbumsQuery.Data.User.FollowedAlbum.Artist artist:album.artists()){
+                                    newAlbum.getArtists().add(new Artist(artist.name()));
+                                }
+                            }
+                            albums.add(newAlbum);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@Nonnull Throwable t) {
+                final String text = t.getMessage();
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         return rootView;
-    }
-
-    private void initializeData() {
-        albums = new ArrayList<>();
-        albums.add(new Album("Album 1", 1995, R.drawable.album));
-        albums.add(new Album("Album 2", 2000, R.drawable.album));
-        albums.add(new Album("Album 3", 2002, R.drawable.album));
-        albums.add(new Album("Album 4", 2002, R.drawable.album));
-        albums.add(new Album("Album 5", 2002, R.drawable.album));
-        albums.add(new Album("Album 6", 2002, R.drawable.album));
-        albums.add(new Album("Album 7", 2002, R.drawable.album));
-        albums.add(new Album("Album 8", 2002, R.drawable.album));
-        albums.add(new Album("Album 9", 2002, R.drawable.album));
-        albums.add(new Album("Album 10", 2002, R.drawable.album));
     }
 
 }
