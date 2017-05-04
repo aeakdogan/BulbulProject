9f5cea2e504c2902d86416df0e2dc3f9e7fbcdda
Enes Akdogan <aeakdogan@gmail.com>
2017-05-05 02:32:09 +0300

    Reusable Song List Activity

---
+++ app/src/main/java/com/bulbulproject/bulbul/service/Globals.java
@@ -0,0 +1,13 @@
+package com.bulbulproject.bulbul.service;
+
+import com.bulbulproject.bulbul.model.MySong;
+
+import java.util.ArrayList;
+
+/**
+ * Created by aeakdogan on 05/05/2017.
+ */
+
+public class Globals {
+    public static ArrayList<MySong> mSongs;
+}
